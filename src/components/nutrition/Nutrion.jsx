839b42e9ab3904af98deeb03866344e06c1f1113
SFERAS <rthyssen73@gmail.com>
2023-04-09 20:52:28 -0300

    nutrition done

--- src/components/nutrition/Nutrion.jsx
+++ src/components/nutrition/Nutrion.jsx
@@ -1,13 +1,6 @@
 import React from 'react'
 import "./Nutrion.scss"
-import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 import picture from "../../images/nutrition.jpg"
-import ntr1 from "../../images/ntr1.jpg"
-import ntr2 from "../../images/ntr2.jpg"
-import ntr3 from "../../images/ntr3.jpg"
-import ntr4 from "../../images/ntr4.jpg"
-import ntr5 from "../../images/ntr5.jpg"
 
 function Nutrion() {
 	return (
@@ -27,8 +20,49 @@ function Nutrion() {
 				</div>
 			</div>
 
+			<div className="nutrition__tips--container">
+				<div className="images"><div className="img1 img"></div>
+					<div className="tips__container tips__container1">
+						<h3>Lorem, ipsum.</h3>
+						<p className='tips__text1'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque sequi aut repellat, laudantium nobis illum placeat repudiandae adipisci molestiae sint, id natus necessitatibus asperiores omnis nemo vero, fugiat dolorem! Incidunt!</p>
+					</div>
+				</div>
 
-			
+				<div className="images"><div className="img2 img"></div>
+					<div className="tips__container tips__container2">
+						<h3>Lorem, ipsum.</h3>
+						<p className='tips__text2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, dolore in expedita inventore sequi eaque, unde est odio ullam ratione libero omnis perferendis aspernatur quas magni incidunt quae. Veritatis, libero?</p>
+					</div>
+				</div>
+
+				<div className="images"><div className="img3 img"></div>
+				<div className="tips__container tips__container3">
+						<h3>Lorem, ipsum.</h3>
+						<p className='tips__text3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias obcaecati repellendus asperiores culpa aliquam dolore ipsum, aperiam repellat, nostrum ipsa tenetur tempore doloremque autem? Hic voluptatum quis quod. Culpa, voluptates?</p>
+					</div>				
+				</div>
+
+				<div className="images"><div className="img4 img"></div>
+				<div className="tips__container tips__container4">
+						<h3>Lorem, ipsum.</h3>
+						<p className='tips__text4'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam quidem, vel iusto commodi error laboriosam! Harum saepe nemo quo. Obcaecati veniam nesciunt maiores. Vero, sit ducimus fugit rem ut illum!</p>
+					</div>				
+				</div>
+
+				<div className="images"><div className="img5 img"></div>
+				<div className="tips__container tips__container5">
+						<h3>Lorem, ipsum.</h3>
+						<p className='tips__text5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam fugit sit totam ipsa quasi! Beatae deleniti pariatur aperiam quasi, nostrum nobis cupiditate quidem consequuntur cumque autem doloribus exercitationem ducimus eligendi.</p>
+					</div>				
+				</div>
+
+				<div className="images"><div className="img6 img"></div>
+				<div className="tips__container tips__container6">
+						<h3>Lorem, ipsum.</h3>
+						<p className='tips__text6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam consectetur nesciunt exercitationem sit maxime eum consequuntur vel illum veniam eius expedita, vero iste laboriosam impedit repudiandae minima, dicta asperiores saepe?</p>
+					</div>				
+				</div>
+			</div>
 
 		</div>
 	)
