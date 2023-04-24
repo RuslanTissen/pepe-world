22650d1e7f22ed7aa510c0201594abd0acb33741
SFERAS <rthyssen73@gmail.com>
2023-04-24 10:56:16 -0300

    header text changed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import './Header.scss'
 import { GiHamburgerMenu } from 'react-icons/gi';
 import AnchorLink from 'react-anchor-link-smooth-scroll-v2';
-import {GiBodyBalance} from "react-icons/gi"
+// import {GiBodyBalance} from "react-icons/gi"
 
 function Header() {
 	const [show, setShow] = useState(false)
@@ -11,18 +11,19 @@ function Header() {
 		<div className="nav">
 			<ul className='ul-first'>
 				{/* <AnchorLink href="#foto" className="logo" style={{fontSize:"1.5em"}}><AiOutlineHome /></AnchorLink> */}
-				<li className="logo"><AnchorLink href="#foto">PIPI <GiBodyBalance/> SPORT</AnchorLink></li>
-				<li className="alq"><AnchorLink href="#philosophie">STRONG</AnchorLink> </li>
-				<li className="detalles"><AnchorLink href="#exercise">RECOVERY</AnchorLink></li>
+				{/* <li className="logo"><AnchorLink href="#foto">PIPI <GiBodyBalance/> SPORT</AnchorLink></li> */}
+				<li className="logo"><AnchorLink href="#foto">HOME</AnchorLink></li>
+				<li className="alq"><AnchorLink href="#philosophie">ABOUT ME</AnchorLink> </li>
+				<li className="detalles"><AnchorLink href="#exercise">STRENGTH</AnchorLink></li>
 				<li className="contacto"><AnchorLink href="#nutrition">NUTRITION</AnchorLink></li>
-				<li className='personal'><AnchorLink href="#stretching">STRETCHING</AnchorLink></li>
+				<li className='personal'><AnchorLink href="#stretching">GET ON TOUCH</AnchorLink></li>
 				<GiHamburgerMenu style={{ fontSize: "1.5em" }} onClick={() => setShow(!show)} />
 			</ul >
 			{show && <ul className='ul-second'>
-				<li className="alq"><AnchorLink href="#philosophie">STRONG</AnchorLink> </li>
-				<li className="detalles"><AnchorLink href="#exercise">DETALLES</AnchorLink></li>
+				<li className="alq"><AnchorLink href="#philosophie">ABOUT ME</AnchorLink> </li>
+				<li className="detalles"><AnchorLink href="#exercise">STRENGTH</AnchorLink></li>
 				<li className="contacto"><AnchorLink href="#nutrition">NUTRITION</AnchorLink></li>
-				<li className="personal"><AnchorLink href="#stretching">STRETCHING</AnchorLink></li>
+				<li className="personal"><AnchorLink href="#stretching">GET ON TOUCH</AnchorLink></li>
 			</ul>}
 		</div>
 	)
