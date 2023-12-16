f89fc1e82788c46792c9f398a5a41e546ffadf46
SFERAS <rthyssen73@gmail.com>
2023-12-16 20:57:58 -0300

    photo adjusted

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,22 +1,33 @@
 import React, { useState } from 'react'
 import './Header.scss'
 import { GiHamburgerMenu } from 'react-icons/gi';
+import { RxCross1 } from 'react-icons/rx';
 import AnchorLink from 'react-anchor-link-smooth-scroll-v2';
 
 function Header() {
 	const [show, setShow] = useState(false)
+	const [active, setActive] = useState(true)
+
+	function myFunction() {
+		setActive(!active)
+		setShow(!show)
+	}
+
+const scrollToTheTop = () => {
+	window.scrollTo({top: 0, behavior: 'smooth'})
+	}
 
 	return (
-		<div className="nav">
+		<div className="nav" >
 			<ul className='ul-first'>
-				<li className="logo"><AnchorLink href="#foto">HOME</AnchorLink></li>
+				<li className="logo"><AnchorLink onClick={scrollToTheTop} href="#foto">HOME</AnchorLink></li>
 				<li className="alq"><AnchorLink href="#philosophie">ABOUT ME</AnchorLink> </li>
 				<li className="detalles"><AnchorLink href="#exercise">STRENGTH</AnchorLink></li>
 				<li className="contacto"><AnchorLink href="#nutrition">NUTRITION</AnchorLink></li>
 				<li className='personal'><AnchorLink href="#stretching">GET ON TOUCH</AnchorLink></li>
 				<GiHamburgerMenu style={{ fontSize: "1.5em" }} onClick={() => setShow(!show)} />
 			</ul >
-			{show && <ul className='ul-second'>
+			{show && <ul className='ul-second' onClick={() => myFunction()}>
 				<li className="alq"><AnchorLink href="#philosophie">ABOUT ME</AnchorLink> </li>
 				<li className="detalles"><AnchorLink href="#exercise">STRENGTH</AnchorLink></li>
 				<li className="contacto"><AnchorLink href="#nutrition">NUTRITION</AnchorLink></li>
